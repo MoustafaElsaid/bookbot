cb669deb0b975a3fa8a5b8ff7cc5e79f5affa3b8
MoustafaElsaid <mk.elsaid@icloud.com>
2025-03-14 17:01:34 +0100

    modified stats.py file

--- stats.py
+++ stats.py
@@ -1,9 +1,12 @@
 import os
+def read_book (filepath):
+    with open(filepath) as file:
+        file_contents = file.read()
+        return file_contents
 def count_words (filepath):
-    with open(filepath) as f:
-        file_contents = f.read()
-        words = file_contents.split() 
+    words = read_book(filepath).split()
     return len(words)
+
 def test():
     path = os.path.expanduser("~/Documents/Studies/Coding/Boot.Dev/github.com/bookbot/books/frankenstein.txt")
     print (count_words(path))
