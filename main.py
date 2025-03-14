8ca584706e67c759472d0819fb09966b50f2d298
MoustafaElsaid <mk.elsaid@icloud.com>
2025-03-14 23:02:30 +0100

    raising my own error for handling sys.argv

--- main.py
+++ main.py
@@ -2,21 +2,25 @@
 from stats import count_words
 from stats import count_individual_chars
 from stats import sort_it_out
-import os
+import sys
 # ---------------------------------> main function
 def main ():
-    filepath = os.path.expanduser("~/Documents/Studies/Coding/Boot.Dev/github.com/bookbot/books/frankenstein.txt")
-    num_words = count_words(filepath)
-    chars = count_individual_chars(filepath)
-    chars_sorted = sort_it_out(chars)
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {filepath}...")
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
-    for item in chars_sorted:
-        char = item["key"]
-        number = item["value"]
-        if char.isalpha():
-            print(f"{char}: {number}")
-    print("============= END ===============")
+
+    try:
+        filepath = sys.argv[1]
+        num_words = count_words(filepath)
+        chars = count_individual_chars(filepath)
+        chars_sorted = sort_it_out(chars)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {filepath}...")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        for item in chars_sorted:
+            char = item["key"]
+            number = item["value"]
+            if char.isalpha():
+                print(f"{char}: {number}")
+        print("============= END ===============")
+    except Exception as e:
+        print(f"{e}!\nUsage: python3 main.py <path_to_book>")
 main()
