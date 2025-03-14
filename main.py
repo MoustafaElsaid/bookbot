42c4726f486d9df8d048dc0c49c04161bd068715
MoustafaElsaid <mk.elsaid@icloud.com>
2025-03-14 18:15:48 +0100

    modified stats.py file

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+# ---------------------------------> import
 from stats import count_words
+from stats import count_individual_chars
 import os
+# ---------------------------------> main function
 def main ():
     filepath = os.path.expanduser("~/Documents/Studies/Coding/Boot.Dev/github.com/bookbot/books/frankenstein.txt")
     num_words = count_words(filepath)
+    chars = count_individual_chars(filepath)
     print(f"{num_words} words found in the document")
+    print(chars)
 
 main()
--- stats.py
+++ stats.py
@@ -1,14 +1,34 @@
+# ---------------------------------> import libraries
 import os
+# ---------------------------------> read book file
 def read_book (filepath):
+    filepath = os.path.expanduser(filepath)
     with open(filepath) as file:
         file_contents = file.read()
-        return file_contents
+    return file_contents
+# ---------------------------------> count words in book
 def count_words (filepath):
     words = read_book(filepath).split()
     return len(words)
+# ---------------------------------> count characters
+def count_individual_chars (filepath):
+    filepath = os.path.expanduser(filepath)
+    book = read_book(filepath).lower()
+    chars_dict = {}
+    for char in book:
+        if char in chars_dict:
+            chars_dict[char] += 1
+        else:
+            chars_dict[char] = 1
+    return chars_dict
+# ---------------------------------> sorting function
+def sort_dict(chars_dict):
 
+    return
+# ---------------------------------> Test the function
 def test():
     path = os.path.expanduser("~/Documents/Studies/Coding/Boot.Dev/github.com/bookbot/books/frankenstein.txt")
     print (count_words(path))
+    print (count_individual_chars(path))
 
-test() 
+#test()
